98ecf4fe6aed00eda1542a2244e6ecd54277b3cb
Rindi Tiara PA <rinditiarapa@gmail.com>
2016-11-24 06:37:48 +0700

    Edited xml

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl3102030/duniaanakislami/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl3102030/duniaanakislami/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import id.sch.smktelkom_mlg.project.xiirpl3102030.duniaanakislami.model.hijaiyah;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,23 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.buttonLagu).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.IBplay).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, LaguActivity.class));
-            }
-        });
-
-        findViewById(R.id.buttonDoa).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, DoaActivity.class));
-            }
-        });
-        findViewById(R.id.buttonHuruf).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, hijaiyah.class));
+                startActivity(new Intent(MainActivity.this, MenuAwal.class));
             }
         });
     }
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl3102030/duniaanakislami/MenuAwal.java
@@ -0,0 +1,37 @@
+package id.sch.smktelkom_mlg.project.xiirpl3102030.duniaanakislami;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import id.sch.smktelkom_mlg.project.xiirpl3102030.duniaanakislami.model.hijaiyah;
+
+public class MenuAwal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_awal);
+
+        findViewById(R.id.buttonLagu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuAwal.this, LaguActivity.class));
+            }
+        });
+
+        findViewById(R.id.buttonDoa).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuAwal.this, DoaActivity.class));
+            }
+        });
+        findViewById(R.id.buttonHuruf).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuAwal.this, hijaiyah.class));
+            }
+        });
+    }
+}
